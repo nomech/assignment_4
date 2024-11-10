860ec0477bb84c2636edbba4f8a890a95db3b2bf
nomech <mech1803@gmail.com>
2024-11-10 19:34:59 +0100

    Implement dynamic component list rendering and enhance drawer functionality

---
+++ src/js/data.js
@@ -0,0 +1,96 @@
+export const data = [
+  {
+    title: "Components",
+    components: [
+      {
+        data: "Buttons",
+        types: [
+          {
+            type: "Primary",
+            description:
+              "The primary button is used to indicate the main action on a page.",
+            code: `<button class="button button--primary">Primary</button>`,
+            size: ["Small", "Medium", "Large"],
+          },
+          {
+            type: "Secondary",
+            description:
+              "The secondary button is used to indicate a secondary action on a page.",
+            code: `<button class="button button--secondary">Secondary</button>`,
+          },
+          {
+            type: "Tertiary",
+            description:
+              "The tertiary button is used to indicate a tertiary action on a page.",
+            code: `<button class="button button--tertiary">Tertiary</button>`,
+          },
+          {
+            type: "Disabled",
+            description:
+              "The disabled button is used to indicate a disabled action on a page.",
+            code: `<button class="button button--disabled" disabled>Disabled</button>`,
+          },
+        ],
+      },
+      {
+        component: "Inputs",
+        types: [
+          {
+            type: "Text",
+            description: "The text input is used to collect text data.",
+            code: `<input type="text" class="input input--text" placeholder="Text input">`,
+          },
+          {
+            type: "Email",
+            description: "The email input is used to collect email data.",
+            code: `<input type="email" class="input input--email" placeholder="Email input">`,
+          },
+          {
+            type: "Password",
+            description: "The password input is used to collect password data.",
+            code: `<input type="password" class="input input--password" placeholder="Password input">`,
+          },
+          {
+            type: "Checkbox",
+            description: "The checkbox input is used to collect boolean data.",
+            code: `<input type="checkbox" class="input input--checkbox">`,
+          },
+        ],
+      },
+      {
+        component: "Cards",
+        types: [
+          {
+            type: "Primary",
+            description:
+              "The primary card is used to display the main content on a page.",
+            code: `<div class="card card--primary">Primary card</div>`,
+          },
+          {
+            type: "Secondary",
+            description:
+              "The secondary card is used to display secondary content on a page.",
+            code: `<div class="card card--secondary">Secondary card</div>`,
+          },
+        ],
+      },
+      {
+        component: "Modals",
+        types: [
+          {
+            type: "Primary",
+            description:
+              "The primary modal is used to display the main content on a page.",
+            code: `<div class="modal modal--primary">Primary modal</div>`,
+          },
+          {
+            type: "Secondary",
+            description:
+              "The secondary modal is used to display secondary content on a page.",
+            code: `<div class="modal modal--secondary">Secondary modal</div>`,
+          },
+        ],
+      },
+    ],
+  },
+];
--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,84 @@
+import { data } from "./data.js";
+
 const drawerButton = document.querySelector(".components__drawer-button");
 const chevron = document.querySelector(".components__drawer-chevron");
 
 const components = document.querySelector(".components");
-const componentsList = document.querySelector(".components-list");
-const componentsSublist = document.querySelector(".components-sublist");
-
-
-drawerButton.addEventListener("click", function() {
-
-    chevron.classList.toggle("components__drawer-chevron");
-    chevron.classList.toggle("components__drawer-chevron--rotated");
-});
+const componentsList = document.querySelector(".components__list");
+const componentsSublist = document.querySelector(".components__sublist");
 
 const search = document.querySelector(".page-header__search");
 
-search.addEventListener('blur', function() {
-  search.value = '';
+const createListItem = (title) => {
+  console.log(title);
+  const listItem = document.createElement("li");
+  listItem.classList.add("components__item");
+
+  console.log(componentsList);
+  console.log(listItem);
+  componentsList.appendChild(listItem);
+
+  const titleGroup = document.createElement("div");
+  titleGroup.classList.add("components__title-group");
+  listItem.appendChild(titleGroup);
+
+  const itemTitile = document.createElement("h3");
+  itemTitile.classList.add("components__title");
+  itemTitile.textContent = title;
+  titleGroup.appendChild(itemTitile);
+
+  const chevron = document.createElement("img");
+  chevron.classList.add("components__chevron");
+  chevron.src = "./assets/icons/chevron-lsolid.svg";
+  chevron.alt = "chevron";
+  titleGroup.appendChild(chevron);
+};
+
+constCreate
+
+data.forEach((item) => {
+  console.log(item);
+  createListItem(item.title);
 });
+
+const triggerDrawer = () => {
+  components.classList.toggle("retracted");
+  chevron.classList.toggle("components__drawer-chevron");
+  chevron.classList.toggle("components__drawer-chevron--rotated");
+};
+
+const onUnfocusSearch = () => {
+  search.value = "";
+};
+
+const componentsTitleGroup = document.querySelectorAll(
+  ".components__title-group"
+);
+
+const collapseComponents = (e) => {
+  let children = e.currentTarget.children;
+  let subListTarget = e.currentTarget.nextElementSibling;
+
+  for (let child of children) {
+    if (child.classList.contains("components__chevron")) {
+      child.classList.toggle("components__chevron--rotated");
+    }
+  }
+
+  if (subListTarget.classList.contains("open")) {
+    subListTarget.classList.toggle("open");
+    subListTarget.style.height = "0px";
+    return;
+  } else {
+    const scrollHeight = componentsSublist.scrollHeight;
+    subListTarget.style.height = `${scrollHeight}px`;
+    subListTarget.classList.toggle("open");
+  }
+};
+
+componentsTitleGroup.forEach((chevron) => {
+  chevron.addEventListener("click", collapseComponents);
+});
+
+search.addEventListener("blur", onUnfocusSearch);
+drawerButton.addEventListener("click", triggerDrawer);
